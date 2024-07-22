003b433a4262100e4ddbb53d47226d07476c6719
Reenta <reenta.kim@gmail.com>
2024-07-22 17:57:13 +0900

    현재위치 가져오기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {useEffect} from "react";
 
 // 1. 앱이 실행되자마자 현재위치 기반의 날씨가 보인다.
 // 2. 날씨정보에는 도시, 섭씨, 화씨, 날씨상태
@@ -7,7 +8,34 @@ import './App.css';
 // 5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
 // 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 function App() {
+    const getCurrentLocation = () => {
+        console.log('getCurrentLocation');
+        navigator.geolocation.getCurrentPosition(position => {
+           let lat = position.coords.latitude;
+           let lon = position.coords.longitude;
+            console.log(lat, lon);
+            // fetchWeather(lat,lon);
+        });
+    }
+
+    // const fetchWeather = async (lat, lon) => {
+    //     console.log(lat,lon);
+    //     try {
+    //         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    //         const data = await response.json();
+    //         console.log(data);
+    //     } catch (error) {
+    //         console.error("Error fetching weather:", error);
+    //     }
+    // };
+
+    useEffect(() => {
+        getCurrentLocation();
+    }, []);
+
     return (
         <div>
             HI!
