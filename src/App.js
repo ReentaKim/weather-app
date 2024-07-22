559cc26431e57578013124bfe11d1d486fccc84a
Reenta <reenta.kim@gmail.com>
2024-07-22 18:46:32 +0900

    날씨데이터 보여주기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import WeatherBox from "./component/WeatherBox";
 import WeatherButton from "./component/WeatherButton";
 
@@ -14,22 +14,22 @@ import WeatherButton from "./component/WeatherButton";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
+
+    const [weather, setWeather] = useState(null);
+
     const getCurrentLocation = () => {
-        console.log('getCurrentLocation');
         navigator.geolocation.getCurrentPosition(position => {
             let lat = position.coords.latitude;
             let lon = position.coords.longitude;
-            console.log(lat, lon);
             getWeatherByCurrentLocation(lat, lon);
         });
     }
 
     const getWeatherByCurrentLocation = async (lat, lon) => {
-        console.log(API_KEY)
-        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+        let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
         let response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        setWeather(data);
 
     };
 
@@ -40,7 +40,7 @@ function App() {
     return (
         <div>
             <div className={'container'}>
-                <WeatherBox/>
+                <WeatherBox weather={weather} />
                 <WeatherButton/>
             </div>
         </div>
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const WeatherBox = () => {
+const WeatherBox = ({weather}) => {
+    console.log('weather ? ', weather);
+
+    const celcius = weather?.main.temp;
+    const fahrenheit = (celcius * 9/5) + 32;
+
     return (
         <div className={'weather-box'}>
-            <div>서울</div>
-            <h2>30도 / 230화씨</h2>
-            <h3>맑은 하늘</h3>
+            <div>{weather?.name}</div>
+            <h2>{celcius}C /{fahrenheit}화씨</h2>
+            <h3>{weather?.weather[0].description}</h3>
         </div>
     );
 };
